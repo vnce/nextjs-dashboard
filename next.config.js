e123aae7129736f526a562a1adad1a251b29c9c7
Vince Lacey <vince.lacey@gmail.com>
2024-06-17 22:19:13 -0700

    next.config.js include miniCssExtractPlugin
    Resolve server errors in this missing dep despite this documentation missing from the tutorial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Modify the existing CSS rule to use MiniCssExtractPlugin
+    config.module.rules.forEach((rule) => {
+      if (rule.test && rule.test.toString().includes('css')) {
+        rule.use = [
+          !isServer ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+        ];
+      }
+    });
+
+    // Add MiniCssExtractPlugin to the plugins array
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: isServer
+          ? '[name].css'
+          : 'static/css/[name].[contenthash].css',
+        chunkFilename: isServer
+          ? '[id].css'
+          : 'static/css/[id].[contenthash].css',
+      }),
+    );
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
