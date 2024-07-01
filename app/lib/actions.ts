b218a8f64711e45409c09d247986a93f742d1023
Vince Lacey <vince.lacey@gmail.com>
2024-06-30 22:10:00 -0700

    Handle errors thrown by form validator

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,11 +18,16 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
+  try {
+    const { customerId, amount, status } = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+  } catch (error) {
+    return { message: 'Bad form input.' };
+  }
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
